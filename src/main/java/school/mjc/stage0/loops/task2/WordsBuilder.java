63b713135bc903cacba96cc13fbf71e13e72e146
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-09-20 13:05:36 +0400

    Update WordsBuilder.java

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-
+        int n = 0;
+        StringBuilder phrase = new StringBuilder();
+        while (n < chars.length){
+            phrase.append(chars[n]);
+            n++;
+        }
+        System.out.println(phrase);
     }
 }
