6649ce149aea5cc6336975e1d77cdc589ef9a673
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-09-20 15:26:16 +0400

    Update FactorialNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -4,6 +4,9 @@ public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
         int n = 1;
         int k = 1;
+        if(printToInclusive == 0){
+            System.out.println(1);
+        }
         while (n < printToInclusive+1){
             k = n*k;
             System.out.println(k);
